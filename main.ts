0ea61b75c7c2a721bbf16545bf377a8e8203526b
Alleshka <alleshka94@gmail.com>
2025-05-07 13:50:59 +0400

    Change a report type

--- main.ts
+++ main.ts
@@ -86,12 +86,17 @@ export default class AGAnkiCardCreator extends Plugin {
 			}
 		}
 
-		new Notice(`${added.length} notes were adeed`);
+		if (false) {
+			new Notice(`${added.length} notes were adeed`);
 
-		if (notAdded.length != 0) {
-			for (const note of notAdded) {
-				new Notice(`Note '${Object.values(note.ankiNote.fields)[0]}' was't added`);
+			if (notAdded.length != 0) {
+				for (const note of notAdded) {
+					new Notice(`Note '${Object.values(note.ankiNote.fields)[0]}' was't added`);
+				}
 			}
+		}
+		else {
+			new SampleModal(this.app, added, notAdded).open();
 		}
 	}
 
@@ -284,13 +289,53 @@ export default class AGAnkiCardCreator extends Plugin {
 }
 
 class SampleModal extends Modal {
-	constructor(app: App) {
+	private addedItems: NoteInfo[];
+	private notAddedItems: NoteInfo[];
+
+	constructor(app: App, addedItems: NoteInfo[], notAddedItems: NoteInfo[]) {
 		super(app);
+
+		this.addedItems = addedItems;
+		this.notAddedItems = notAddedItems;
 	}
 
 	onOpen() {
 		const { contentEl } = this;
-		contentEl.setText('Woah!');
+
+		contentEl.empty(); // Clear any previous content
+		contentEl.createEl('h2', { text: 'Operation Report' });
+
+		// Added Section
+		const addedDiv = contentEl.createDiv({ cls: 'report-section' });
+		addedDiv.createEl('h3', { text: '✅ Added' });
+		if (this.addedItems.length > 0) {
+			const ul = addedDiv.createEl('ul');
+			this.addedItems.forEach(note => {
+				ul.createEl('li', { text: `${Object.values(note.ankiNote.fields)[0]}` });
+			});
+		} else {
+			addedDiv.createEl('p', { text: 'No items were added.' });
+		}
+
+		// Not Added Section
+		const notAddedDiv = contentEl.createDiv({ cls: 'report-section' });
+		notAddedDiv.createEl('h3', { text: '❌ Not Added' });
+		if (this.notAddedItems.length > 0) {
+			const ul = notAddedDiv.createEl('ul');
+			this.notAddedItems.forEach(note => {
+				ul.createEl('li', { text: `${Object.values(note.ankiNote.fields)[0]}` });
+			});
+		} else {
+			notAddedDiv.createEl('p', { text: 'All items were added successfully.' });
+		}
+
+		// Close button
+		new Setting(contentEl)
+			.addButton(btn =>
+				btn.setButtonText('Close')
+					.setCta()
+					.onClick(() => this.close())
+			);
 	}
 
 	onClose() {
